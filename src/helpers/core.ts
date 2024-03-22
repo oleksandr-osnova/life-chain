43e703fc995eb262641576afd725a929d67b11a6
Oleksandr Osnova <alex.osnova.work@gmail.com>
2024-03-22 22:01:11 +0200

    Fix lint error

--- src/helpers/core.ts
+++ src/helpers/core.ts
@@ -1,6 +1,6 @@
 import nextConfig from "../../next.config.mjs";
 
-export const prepareSrc = src => {
+export const prepareSrc = (src: string) => {
   const { basePath = '' } = nextConfig
   return `${basePath}${src}`
 }

e6f50d3560c71e54024407471b1619785f6f193b
Oleksandr Osnova <alex.osnova.work@gmail.com>
2024-03-22 20:10:10 +0200

    Update workflow

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    basePath: '/life-chain',
+    // basePath: '/life-chain',
     // https://nextjs.org/docs/api-reference/next/image#unoptimized
-    'experimental.images.unoptimized': true,
+    // 'experimental.images.unoptimized': true,
     // No longer experimental as of Next.js v12.3.0
-    'images.unoptimized': true
+    // 'images.unoptimized': true
 };
 
 export default nextConfig;

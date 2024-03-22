ded5f1bec0d82e6863ff51ae95bf16f283ffe797
Oleksandr Osnova <alex.osnova.work@gmail.com>
2024-03-22 19:48:06 +0200

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // distDir: 'aaaForCheck',
     reactStrictMode: true,
+    basePath: '/life-chain',
+    // https://nextjs.org/docs/api-reference/next/image#unoptimized
+    'experimental.images.unoptimized': true,
+    // No longer experimental as of Next.js v12.3.0
+    'images.unoptimized': true
 };
 
 export default nextConfig;
